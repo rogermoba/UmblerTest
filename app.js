03fdd4127d03eb3188d89fdeb7295e6bef848b02
Moba <rogermoba@hotmail.com>
2019-09-03 22:58:33 -0300

    DeusÉTop

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+app.get('*', (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] != 'https') {
+      
+      res.redirect("https://" + req.headers.host + req.url);
+
+  } else {
+      next();
+
+  }
+});
+
