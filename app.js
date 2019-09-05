ad83ee7570c34c48e1d84698950c9227dcbb870d
Moba <rogermoba@hotmail.com>
2019-09-04 22:20:56 -0300

    DeusMeAjudaAiMano

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var app = express();
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World! ' + new Date().toLocaleTimeString());
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 app.get('*', (req, res, next) => {
   if (req.headers['x-forwarded-proto'] != 'https') {
       
@@ -20,4 +11,14 @@ app.get('*', (req, res, next) => {
 
   }
 });
+
+app.get('/', function (req, res) {
+  res.send('Hello World! ' + new Date().toLocaleTimeString());
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
 
